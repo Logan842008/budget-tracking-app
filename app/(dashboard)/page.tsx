1a483d2e54944f5629a9c6bb1a6c7a3ce35da589
Logan Kannan <logan842008@gmail.com>
2024-08-31 05:19:45 +0800

    deploy vercel

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
+import CreateTransactionDialog from "./_components/CreateTransactionDialog";
+import Overview from "./_components/Overview";
+import History from "./_components/History";
+
+async function page() {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  const userSettings = await prisma.userSettings.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (!userSettings) {
+    redirect("/wizard");
+  }
+  return (
+    <div className="h-full bg-background">
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+          <p className="text-2xl font-bold">Hello, {user.firstName}! 👋</p>
+          <div className="flex items-center gap-3">
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-emerald-500 bg-emerald-400 dark:bg-emerald-950 hover:bg-emerald-500 dark:hover:bg-emerald-700"
+                >
+                  New Income 🤑
+                </Button>
+              }
+              type="income"
+            />
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-rose-500 dark:bg-rose-950 bg-rose-400 hover:bg-rose-500 dark:hover:bg-rose-700"
+                >
+                  New Expense 😤
+                </Button>
+              }
+              type="expense"
+            />
+          </div>
+        </div>
+      </div>
+      <Overview userSettings={userSettings} />
+      <History userSettings={userSettings} />
+    </div>
+  );
+}
+
+export default page;
